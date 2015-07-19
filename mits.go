d05162955d846feb54b539495f2e57f1f462d237
Yu-Han Lin <guesslin1986@gmail.com>
2015-07-19 14:17:51 +0800

    Add: Segment calculate mutual information of given sentence
    	By calculating MI to segment sentence
    	TODO:
    		Merge more than two sentence result

--- mits.go
+++ mits.go
@@ -5,13 +5,26 @@ Package mits implements simple text segmentation by calculate words' mutual info
 package mits
 
 import (
-//	"math"
+	"math"
 )
+
+// Terms contains result of MITS which means "term in string => probability of each term"
+type Terms map[string]float64
+
+// Mits read sentences and translate them into pieces of single char and double chars
+// use
+func Mits() {
+}
+
+// prob returns probability of single word in all documents/sentence
+func prob() {
+}
 
 // CountSingleWord count frequency of each single rune word appear
 // in single file
-func CountSingleWord(sentence []rune) (single map[string]int) {
+func CountSingleWord(sentence []rune) (single map[string]int, count int) {
 	single = make(map[string]int)
+	count = len(sentence)
 	for _, word := range sentence {
 		single[string(word)] += 1
 	}
@@ -19,8 +32,9 @@ func CountSingleWord(sentence []rune) (single map[string]int) {
 }
 
 // CountTwinWord
-func CountTwinWord(sentence []rune) (twin map[string]int) {
+func CountTwinWord(sentence []rune) (twin map[string]int, count int) {
 	twin = make(map[string]int)
+	count = len(sentence) - 1
 	for i, _ := range sentence {
 		if i == len(sentence)-1 {
 			break // last word of sentence
@@ -32,14 +46,16 @@ func CountTwinWord(sentence []rune) (twin map[string]int) {
 }
 
 // CountTermFreq count frequency of single word and twin-word term
-func CountTermFreq(sentence []rune) (single, twin map[string]int) {
+func CountTermFreq(sentence []rune) (single, twin map[string]int, s_len, t_len int) {
 	single = make(map[string]int)
 	twin = make(map[string]int)
+	s_len = len(sentence)
 	for i, word := range sentence {
 		single[string(word)] += 1
 		if i < len(sentence)-1 {
 			two := sentence[i : i+2]
 			twin[string(two)] += 1
+			t_len += 1
 		}
 	}
 	return
@@ -48,9 +64,29 @@ func CountTermFreq(sentence []rune) (single, twin map[string]int) {
 // CalcMI use single word frequency and twin word term frequency to
 // calculate mutual information value of continue term
 // mutual information value formula:
-//                    p(x[i], x[i+1]) * log(p(x[i], x[i+1])
-// MI(x[i], x[i+1]) = -------------------------------------
-//                           (p(x[i]) * p(x[i+1])))
-func CalcMI(single, twin map[string]int) (mi float64) {
+// 	MI(x[i], x[i+1]) = p(x[i], x[i+1]) * log(p(x[i], x[i+1]) / (p(x[i]) * p(x[i+1])))
+func CalcMI(single, twin map[string]int, word string) (mi float64) {
+	return
+}
+
+// Segment sentence and return segmented terms by mutual information
+// threshold
+func Segment(sentence []rune, theta float64) (terms []string) {
+	terms = make([]string, 0)
+	single, twin, s_len, t_len := CountTermFreq(sentence)
+	var term = string(sentence[0])
+	for i := 1; i <= len(sentence)-1; i++ {
+		x2 := string(sentence[i-1 : i+1])
+		posofx2 := float64(twin[x2]) / float64(t_len)
+		posofxi := float64(single[string(sentence[i-1])]) / float64(s_len)
+		posofxj := float64(single[string(sentence[i])]) / float64(s_len)
+		mi := posofx2 * math.Log(posofx2/(posofxi*posofxj))
+		if mi < theta {
+			terms = append(terms, term)
+			term = string(sentence[i])
+		} else {
+			term += string(sentence[i])
+		}
+	}
 	return
 }
--- mits_test.go
+++ mits_test.go
@@ -9,15 +9,19 @@ func TestCountSingleWord(t *testing.T) {
 	cases := []struct {
 		sentence []rune
 		result   map[string]int
+		single   int
 	}{
-		{sentence: []rune("今天的天氣真的很好的說"), result: map[string]int{"今": 1, "天": 2, "的": 3, "氣": 1, "真": 1, "很": 1, "好": 1, "說": 1}},
-		{sentence: []rune("是不是這樣的日子裡，你才會這樣的想起我"), result: map[string]int{"會": 1, "的": 2, "日": 1, "子": 1, "才": 1, "想": 1, "我": 1, "這": 2, "不": 1, "樣": 2, "裡": 1, "，": 1, "是": 2, "起": 1, "你": 1}},
+		{sentence: []rune("今天的天氣真的很好的說"), result: map[string]int{"今": 1, "天": 2, "的": 3, "氣": 1, "真": 1, "很": 1, "好": 1, "說": 1}, single: 11},
+		{sentence: []rune("是不是這樣的日子裡，你才會這樣的想起我"), result: map[string]int{"會": 1, "的": 2, "日": 1, "子": 1, "才": 1, "想": 1, "我": 1, "這": 2, "不": 1, "樣": 2, "裡": 1, "，": 1, "是": 2, "起": 1, "你": 1}, single: 19},
 	}
 	for _, c := range cases {
-		result := CountSingleWord(c.sentence)
+		result, count := CountSingleWord(c.sentence)
 		eq := reflect.DeepEqual(result, c.result)
 		if !eq {
 			t.Errorf("\nExpect\t%+v\nGet\t%+v\n", c.result, result)
+		}
+		if count != c.single {
+			t.Errorf("Except length\t%d\nGet\t%d\n", c.single, count)
 		}
 	}
 }
@@ -26,14 +30,18 @@ func TestCountTwinWord(t *testing.T) {
 	cases := []struct {
 		sentence []rune
 		result   map[string]int
+		twin     int
 	}{
-		{sentence: []rune("今天的天氣真的很好的說"), result: map[string]int{"今天": 1, "天的": 1, "的天": 1, "天氣": 1, "氣真": 1, "真的": 1, "的很": 1, "很好": 1, "好的": 1, "的說": 1}},
+		{sentence: []rune("今天的天氣真的很好的說"), result: map[string]int{"今天": 1, "天的": 1, "的天": 1, "天氣": 1, "氣真": 1, "真的": 1, "的很": 1, "很好": 1, "好的": 1, "的說": 1}, twin: 10},
 	}
 	for _, c := range cases {
-		result := CountTwinWord(c.sentence)
+		result, count := CountTwinWord(c.sentence)
 		eq := reflect.DeepEqual(result, c.result)
 		if !eq {
 			t.Errorf("\nExcept\t%+v\nGet\t%+v\n", c.result, result)
+		}
+		if count != c.twin {
+			t.Errorf("Except length\t%d\nGet\t%d\n", c.twin, count)
 		}
 	}
 }
@@ -42,8 +50,9 @@ func TestCountTermFreq(t *testing.T) {
 	cases := []struct {
 		sentence                 []rune
 		singleResult, twinResult map[string]int
+		single, twin             int
 	}{
-		{sentence: []rune("今天的天氣真的很好的說"), singleResult: map[string]int{"今": 1, "天": 2, "的": 3, "氣": 1, "真": 1, "很": 1, "好": 1, "說": 1}, twinResult: map[string]int{"今天": 1, "天的": 1, "的天": 1, "天氣": 1, "氣真": 1, "真的": 1, "的很": 1, "很好": 1, "好的": 1, "的說": 1}},
+		{sentence: []rune("今天的天氣真的很好的說"), singleResult: map[string]int{"今": 1, "天": 2, "的": 3, "氣": 1, "真": 1, "很": 1, "好": 1, "說": 1}, twinResult: map[string]int{"今天": 1, "天的": 1, "的天": 1, "天氣": 1, "氣真": 1, "真的": 1, "的很": 1, "很好": 1, "好的": 1, "的說": 1}, single: 11, twin: 10},
 		//		{sentence: []rune("是不是這樣的日子裡，你才會這樣的想起我"), singleResult: map[string]int{"會": 1, "的": 2, "日": 1, "子": 1, "才": 1, "想": 1, "我": 1, "這": 2, "不": 1, "樣": 2, "裡": 1, "，": 1, "是": 2, "起": 1, "你": 1}},
 	}
 	for _, c := range cases {
